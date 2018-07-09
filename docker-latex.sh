ff1314da5dfef3af97b34a31dc2170254a1f5e95
Tobias Doernbach <t.fromm@jacobs-university.de>
2018-07-09 18:58:01 +0200

    reverted to texlive 2017 since the 2018 Docker image produces some weird compilation errors

--- docker-latex.sh
+++ docker-latex.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker run -t --rm -v `pwd`:/home -w /home sumdoc/texlive-2018 /bin/bash -c "pdflatex -shell-escape -synctex=1 -interaction=nonstopmode $1.tex"
+docker run -t --rm -v `pwd`:/home -w /home sumdoc/texlive-2017 /bin/bash -c "pdflatex -shell-escape -synctex=1 -interaction=nonstopmode $1.tex"
