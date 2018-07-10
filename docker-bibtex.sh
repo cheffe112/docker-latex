971b26443b31e9a6b4f8de3e635df8fe1fe3261d
Tobias Doernbach <t.fromm@jacobs-university.de>
2018-07-10 16:33:20 +0200

    automatically run biber when biblatex fails

--- docker-bibtex.sh
+++ docker-bibtex.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-docker run -t --rm -v `pwd`:/home -w /home sumdoc/texlive-2017 /bin/bash -c "bibtex $1"
+# automatically tries to use biber if biblatex fails - this makes both bibtex tools work
+docker run -t --rm -v `pwd`:/home -w /home sumdoc/texlive-2017 /bin/bash -c "if ! bibtex $1; then biber $1; fi"
