c5d4f573fd56c3e65fc517317bb0a7d6fe9108c3
Tobias Doernbach <t.fromm@jacobs-university.de>
2018-09-26 15:42:40 +0200

    added word and character count after latex run

--- docker-latex.sh
+++ docker-latex.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
 docker run -t --rm -v `pwd`:/home -w /home sumdoc/texlive-2017 /bin/bash -c "pdflatex -shell-escape -synctex=1 -interaction=nonstopmode $1.tex"
+words=`pdftotext $1.pdf - -q | tr -d '.' | wc -w`
+chars=`pdftotext $1.pdf - -q | tr -d '.' | wc -c`
+echo "#words: $words, #characters: $chars"
